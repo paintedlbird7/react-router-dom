67507ede94e139f55c3844a84fde189e4ef5acd7
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-03-27 18:41:38 -0700

    navigates between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 // src/App.jsx
 
+import { useState } from 'react';
+import PokemonList from './components/PokemonList/PokemonList';
+import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router';
+
+
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+];
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pokemon, setPokemon] = useState(initialState);
+  return (
+    <>
+      <h1>Pokemon!</h1>
+      <NavBar />
+      <Routes>
+      <Route path="/" element={<h2>Home Page</h2>} />
+      <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />      </Routes>
+    </>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router';
 
+// Nest the App component inside the BrowserRouter component to enable routing:
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
+
